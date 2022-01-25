c19733a6be9fd1ecd7babc4534e654c2200de8c4
Abdallah <abdallah.m@padoa-group.com>
2022-01-25 22:17:15 +0100

    fix(cron) remove space

--- src/services/jobs/update-match-status.job.ts
+++ src/services/jobs/update-match-status.job.ts
@@ -1,7 +1,6 @@
 import { CronJob } from 'cron';
 import { UserService } from 'services/user/user.service';
 
-
 const updateMatchStatusJob = new CronJob('01 07 * * *', async () => {
   await UserService.updateAllUnavailableFromYesterdayToUnchecked();
   console.log('Updated All Unavailable from yesterday to unchecked');
