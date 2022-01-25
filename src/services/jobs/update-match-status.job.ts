4735056c5b29a9b29a08bfb0231f57590c58f6f3
Abdallah <abdallah.m@padoa-group.com>
2022-01-25 10:34:06 +0100

    fix(cron) change cron time

--- src/services/jobs/update-match-status.job.ts
+++ src/services/jobs/update-match-status.job.ts
@@ -2,7 +2,7 @@ import { CronJob } from 'cron';
 import { UserService } from 'services/user/user.service';
 
 
-const updateMatchStatusJob = new CronJob('39 22 * * *', async () => {
+const updateMatchStatusJob = new CronJob('01 07 * * *', async () => {
   await UserService.updateAllUnavailableFromYesterdayToUnchecked();
   console.log('Updated All Unavailable from yesterday to unchecked');
 }, null, true, 'Europe/Paris');
